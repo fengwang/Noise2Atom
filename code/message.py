9655d10853c7d840da91856d227598af4545e424
Feng Wang <wang_feng@live.com>
2020-07-20 11:24:34 +0200

    adding telegram bot messager

---
+++ code/message.py
@@ -0,0 +1,22 @@
+#
+# sending message to telegram
+#
+import os
+
+private_key = 'None' # your private key
+private_id = 'None' # your private id
+
+def send_message( message ):
+    command = f'/usr/bin/curl -s -X POST https://api.telegram.org/{private_key}/sendMessage -d chat_id={private_id} -d text="{message}"'
+    os.system( command )
+    print( '*'*10 )
+
+def send_photo( photo_path ):
+    command = f'/usr/bin/curl -s -X POST https://api.telegram.org/{private_key}/sendPhoto -F chat_id={private_id} -F photo="@{photo_path}"'
+    os.system( command )
+    print( '*'*10 )
+
+if __name__ == '__main__':
+    send_message( 'Test message: message from python' )
+    #send_photo( './10002073854_53dc1acd33_o.jpg' )
+
