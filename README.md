2b61703f6571fb17cb463f233748a6249f69a315
Feng Wang <wang_feng@live.com>
2020-08-06 22:53:54 +0200

    updated mesage

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-# Noise2Atom
+# [Noise2Atom: Unsupervised Denoising for Scanning Transmission Electron Microscopy Images](http://fengwang.github.io/noise2atom/#/)
 
 ----
 
-Source code and dataset for __Noise2Atom: Unsupervised Denoising for Scanning Transmission Electron Microscopy Images__.
+## Requirements:
+
+- Python 3.8.5
+- Tensorflow 2.3.0
+- opencv 4.4.0
+- python-imageio 2.8.0
+- python-tqdm 4.48.2
+- python-scikit-image 0.17.2
+- python-numpya 1.19.1
+- python-tifffile 2020.7.24
 
 
+We use [a telegram bot](https://core.telegram.org/bots) to monitor the real time training process.
+The private key and private chat id in file `code/message.py` should be updated ahead of training.
+Or you can just empty  the code of the `send_message` and `send_file` in this file.
 
+kkk
 
 
 
--- code/message.py
+++ code/message.py
@@ -3,15 +3,28 @@
 #
 import os
 
-private_key = 'None' # your private key
-private_id = 'None' # your private id
+private_key = None # your private key
+private_id = None # your private id
+
 
 def send_message( message ):
+    global private_key
+    global private_id
+
+    if private_key is None or private_id is None:
+        return 0
+
     command = f'/usr/bin/curl -s -X POST https://api.telegram.org/{private_key}/sendMessage -d chat_id={private_id} -d text="{message}"'
     os.system( command )
     print( '*'*10 )
 
 def send_photo( photo_path ):
+    global private_key
+    global private_id
+
+    if private_key is None or private_id is None:
+        return 0
+
     command = f'/usr/bin/curl -s -X POST https://api.telegram.org/{private_key}/sendPhoto -F chat_id={private_id} -F photo="@{photo_path}"'
     os.system( command )
     print( '*'*10 )
